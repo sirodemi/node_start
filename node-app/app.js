c9bba82295aa7e63863209902e38370d27d5e012
ShiroIbi <pres@sunyou.co.jp>
2020-01-31 18:27:56 +0900

    P.104まで

--- node-app/app.js
+++ node-app/app.js
@@ -7,10 +7,14 @@ const index_page = fs.readFileSync('./index.ejs','UTF-8');
 var server = http.createServer(getFromClient);
 
 server.listen(3000);
-console.log('server start')
+console.log('server start');
 
 function getFromClient(request,response){
-    var content = ejs.render(index_page);
+	var content = ejs.render(index_page,
+		{
+            title:"Index",
+            content:"sample page",
+    });
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write(content);
     response.end();
