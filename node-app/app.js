600bc68d05266a52b9befebc76b5022070284d4b
Shiro <pres@sunyou.co.jp>
2020-01-31 16:19:34 +0900

    EJS組込成功！

--- node-app/app.js
+++ node-app/app.js
@@ -1,5 +1,8 @@
 const http = require('http');
 const fs = require('fs')
+const ejs = require('ejs');
+
+const index_page = fs.readFileSync('./index.ejs','UTF-8');
 
 var server = http.createServer(getFromClient);
 
@@ -7,16 +10,9 @@ server.listen(3000);
 console.log('server start')
 
 function getFromClient(request,response){
-    fs.readFile('./index.html','UTF-8',
-        (error,data)=>{
-            var content = data.
-                replace(/dummy_title/g, 'タイトル').
-                replace(/dummy_content/g, 'コンテンツ');
-
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(content);
-            response.end();
-        }
-    );
+    var content = ejs.render(index_page);
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(content);
+    response.end();
 }
 
