37fef1eedeb694463ec3775f76329678f2ff1f9c
ShiroIbi <pres@sunyou.co.jp>
2020-01-31 23:00:43 +0900

    別のページにメッセージを送信

--- node-app/app.js
+++ node-app/app.js
@@ -1,7 +1,8 @@
 const http = require('http');
-const fs   = require('fs')
+const fs   = require('fs');
 const ejs  = require('ejs');
-const url  = require('url')
+const url  = require('url');
+const qs = require('querystring');
 
 const index_page = fs.readFileSync('./index.ejs','utf8');
 const other_page = fs.readFileSync('./other.ejs','utf8');
@@ -18,18 +19,11 @@ function getFromClient(request,response){
     
     switch(url_parts.pathname){
         case '/':
-            var content = "this is index page. ";
-            var query = url_parts.query;
-        //    if(query.msg != undefined){
-                var query_obj = content += 'you send [' + query.msg + ']';
-        //    }
-            var content= ejs.render(index_page,{
-                title:"Index",
-                content:content,
-            });
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(content);
-            response.end();
+            response_index(request,response);
+            break;
+
+        case '/other':
+            response_other(request,response);
             break;
 
         case '/style.css':
@@ -46,3 +40,47 @@ function getFromClient(request,response){
     }
 }
 
+function response_index(request,response){
+    var msg = "this is Index page"
+    var content = ejs.render(index_page,{
+        title:"Index",
+        content:msg,
+    });
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(content);
+    response.end();
+}
+
+function response_other(request,response){
+    var msg = "OTHER PAGE!!!";
+    if(request.method == 'POST'){
+        var body = '';
+
+        request.on('data',(data) => {
+            body += data;
+        });
+
+        request.on('end',() => {
+            var post_data = qs.parse(body);
+            msg += ' you wrote ' + post_data.msg;
+            var content = ejs.render(other_page,{
+                title:"OTHER",
+                content:msg,
+            });
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(content);
+            response.end();
+        });
+
+    }else{
+        var msg = "No page";
+        var content = ejs.render(other_page,{
+            title:"OTHER",
+            content:msg,
+        });
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(content);
+        response.end();
+
+    }
+}
