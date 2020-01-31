e26865f067c44cb00a3f12787d7c4bf22fbcef88
ShiroIbi <pres@sunyou.co.jp>
2020-01-31 22:27:44 +0900

    クエリーパラメーターをつけて送信

--- node-app/app.js
+++ node-app/app.js
@@ -1,10 +1,11 @@
 const http = require('http');
-const fs = require('fs')
-const ejs = require('ejs');
-const url = require('url')
+const fs   = require('fs')
+const ejs  = require('ejs');
+const url  = require('url')
 
-const index_page = fs.readFileSync('./index.ejs','UTF-8');
-const style_css = fs.readFileSync('./style.css','UTF-8');
+const index_page = fs.readFileSync('./index.ejs','utf8');
+const other_page = fs.readFileSync('./other.ejs','utf8');
+const style_css  = fs.readFileSync('./style.css','utf8');
 
 var server = http.createServer(getFromClient);
 
@@ -12,17 +13,24 @@ server.listen(3000);
 console.log('server start');
 
 function getFromClient(request,response){
-var url_parts = url.parse(request.url);
+
+    var url_parts = url.parse(request.url, true);
+    
     switch(url_parts.pathname){
         case '/':
+            var content = "this is index page. ";
+            var query = url_parts.query;
+        //    if(query.msg != undefined){
+                var query_obj = content += 'you send [' + query.msg + ']';
+        //    }
             var content= ejs.render(index_page,{
                 title:"Index",
-                content:"sample page",
-        });
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write(content);
-        response.end();
-        break;
+                content:content,
+            });
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(content);
+            response.end();
+            break;
 
         case '/style.css':
             response.writeHead(200, {'Content-Type': 'text/css'});
@@ -34,6 +42,7 @@ var url_parts = url.parse(request.url);
             response.writeHead(200, {'Content-Type': 'text/plain'});
             response.end('no page...');
             break;
+
     }
 }
 
